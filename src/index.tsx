f987071f1b73c06410e6f5b91e28343937bfe841
JayShiznit <12658037+PatrickB-Hub@users.noreply.github.com>
2020-10-07 09:48:33 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+      <h1>Test</h1>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
